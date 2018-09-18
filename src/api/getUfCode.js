4eee7c95e348513c6be9fc8f95fce3c3ddd2521b
luan0ap <luanap.kun@gmail.com>
2018-09-18 15:57:07 -0300

    refactored getUfCode and created a new file Geocoder for instance Geocoder object

--- src/api/getUfCode.js
+++ src/api/getUfCode.js
@@ -1,24 +1,17 @@
-import Google from 'google-maps'
+import Geocoder from 'utils/Geocoder'
 import currentLocation from 'utils/getCurrentLocation'
-
-// Set version script
-Google.VERSION = 'weekly'
+import { get } from 'utils/get'
 
 const regexpCep = /[0-9]{5}-[0-9]{3}/
 
 const getUfCode = () => {
-  Google.load((google) => {
-
-    const Geocoder = new google.maps.Geocoder()
-
-    currentLocation
-      .then(
-        coords => Geocoder.geocode({ location: coords },
-        res => fetch(`https://viacep.com.br/ws/${res[0].formatted_address.match(regexpCep)[0]}/json/`)
-          .then(o => o.json())
-          .then(({ uf }) => uf)
-      ))
-      .catch(err => err)
+  Geocoder().then((geocoder) => {
+    currentLocation.then((coords) => {
+      geocoder.geocode(
+        { location: coords },
+        res => get(`https://viacep.com.br/ws/${res[0].formatted_address.match(regexpCep)[0]}/json/`).then(({ uf }) => console.log(uf))
+      )
+    })
   })
 }
 
